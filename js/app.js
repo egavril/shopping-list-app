6290b4921b1f37717ba9659d375d732f9f285495
Tyler van der Hoeven <hi@tyvdh.com>
2016-03-21 22:31:56 -0400

    made some tweaks

--- js/app.js
+++ js/app.js
@@ -1,66 +1,56 @@
-$(document).ready(function() {	
+$(document).ready(function() {
 
-/* item input */
+  /* item input */
+  $('.new-item').keydown(function(e) {
+    if (e.which == 13) {
+      addItem();
+      $(this).val(''); 
+    }
+  });
 
-	$('#add-new').keydown(function(e){
-	    if (e.which == 13){
-	    addItem();
-	    $(this).val(''); 
-	    }
-	});
+  function addItem() {
+    event.preventDefault();
+    var inputNew = $('.new-item').val();
+  	var newUnchecked = $('<li class="unchecked">'+ inputNew +'</li>');
 
- 	function addItem() {
-		event.preventDefault();
-    	var inputNew = $('#add-new').val();
-   		var newUnchecked = $('<li class="unchecked">'+ inputNew +'</li>');
+  	$('.list-items').prepend(newUnchecked);
 
-   		$('#list-items').prepend(newUnchecked);
-
-   		needAmount++;
-   		updateCount();
-
-   	};
-
-/* manage items */
-
-    $(document).on("click", ".checked", function (){
-    	$(this).remove();
-   		gotAmount--;
-   		updateCount(); 
- 	});
-
-    $(document).on("click", ".unchecked", function() {
-  		$(this).removeClass("unchecked").addClass("checked");
-   
-	   	    needAmount--;
-	        gotAmount++;
-	        updateCount();
-
-/* jquery ui sorting */ 	        
-
-	        $(this).slideUp('fast', function(){
-	        $(this).slideDown('fast').appendTo('#list-items');
-	        });
-	   
-    }); 
-
-	$( "#list-items" ).sortable();
-   	$( "#list-items" ).disableSelection();    
-
-
-/* item counter */
-
-  	var needAmount=4;
-  	var gotAmount=1;
-    
- 	function updateCount(){
-   		$('#need').text(needAmount);
-   		$('#got').text(gotAmount);
-    	$('#total').text(needAmount + gotAmount);
-    
-  	};
-  
+  	needAmount++;
   	updateCount();
+  }
 
+  /* manage items */
+  $(document).on("click", ".checked", function() {
+    $(this).remove();
+  	gotAmount--;
+  	updateCount(); 
+  });
 
-})
+  $(document).on("click", ".unchecked", function() {
+    $(this).removeClass("unchecked").addClass("checked");
+     
+    needAmount--;
+    gotAmount++;
+    updateCount();
+
+    /* jquery ui sorting */ 	        
+    $(this).slideUp('fast', function() {
+      $(this).slideDown('fast').appendTo('.list-items');
+  	});   
+  }); 
+
+  $(".list-items").sortable();
+  $(".list-items").disableSelection();    
+
+  /* item counter */
+  var needAmount = 4;
+  var gotAmount = 1;
+      
+  function updateCount() {
+    $('.need').text(needAmount);
+    $('.got').text(gotAmount);
+    $('.total').text(needAmount + gotAmount);
+  };
+    
+  updateCount();
+});
